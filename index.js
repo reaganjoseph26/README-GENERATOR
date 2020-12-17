276cfa87b3bb474f1091ade0c31dffb5d2fdacdf
Racer <reaganjoseph26@yahoo.com>
2020-12-17 10:39:49 -0500

    inquirer set up

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'name',
+    message: 'What is your projects title? (Required)'
+},
+{
+    type: 'input',
+    name: 'about',
+    message: 'Provide a description of your project',
+},
+{
+    type: 'confirm',
+    name: ''
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
