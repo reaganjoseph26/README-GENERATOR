644cad2a06a81f6dfd4ce1d80f662ec131a1796f
Racer <reaganjoseph26@yahoo.com>
2020-12-17 16:57:07 -0500

    readme updated

--- index.js
+++ index.js
@@ -1,76 +1,120 @@
 // TODO: Include packages needed for this application
-const { fstat } = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [{
+const promtUser = () => {
+    return inquirer
+    .prompt([
+        {
     type: 'input',
     name: 'name',
-    message: 'What is your projects title? (Required)'
-},
+    message: 'What is your projects title? (Required)',
+    validate: nameInput => {
+        if (nameInput) {
+            return true
+        } else {
+            console.log('Please enter a project title')
+            return false;
+        }
+    }
+}
+,
 {
     type: 'input',
     name: 'about',
-    message: 'Provide a description of your project',
+    message: 'Provide a description of your project. (Required)',
+    validate: nameInput => {
+        if (nameInput) {
+            return true
+        } else {
+            console.log('Please enter a description for your project')
+            return false;
+        }
+    }
 },
 {
     type: 'confirm',
     name: 'confirmContentsSection',
     message: 'Would you like to include a Table of Contents section?',
-    default: true 
+    default: true
+},
+{
+    type: 'checkbox',
+    name: 'confirmContentsSection',
+    message: 'Select the sections you will like to include in your Table of Contents',
+    choices: ['Installation', 'Usage', 'Credits', 'License', 'Badges', 'Test' ]
 },
 {
     type: 'input',
     name: 'Installation',
     message: 'Please provide a step-by-step description of how to install your project. (Required)',
-   
+    validate: nameInput => {
+        if(nameInput) {
+            return true
+        } else {
+            console.log('Please provide a step-by-step description of how to install your project.')
+        }
+    }
+
 },
 {
     type: 'confirm',
     name: 'confirmUsage',
     message: 'Would you like to include a Usage section? (ie.. instrcutions, examples of use, screenshots..etc)',
-    default: true 
+    default: true
 },
 {
     type: 'confirm',
     name: 'confirmCredits',
     message: 'Would you like to include a Credits section? (ie.. List of collabortors..etc) ',
-    default: true 
+    default: true
 },
 {
     type: 'input',
     name: 'license',
-    message: 'Please provide a License',
+    message: 'Please provide a License (Required)',
+    validate: nameInput => {
+        if(nameInput) {
+            return true
+        } else {
+            console.log('Please provide a License.')
+        }
+    }
 },
 {
     type: 'confirm',
     name: 'confirmBadges',
     message: 'Would you like to include a Badges section? (ie.. List of collabortors..etc) ',
-    default: true 
+    default: true
 },
 {
     type: 'confirm',
     name: 'confirmContribute',
     message: 'Would you like others developers to contribute to this application? ',
-    default: true 
+    default: true
 },
 {
     type: 'confirm',
     name: 'confirmTest',
     message: 'Would you like to include a Test section? (ie.. List of collabortors..etc) ',
-    default: true 
+    default: true
 }
-];
+])
+}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    return new Promise((resolve, rejects) => {
-        fs.wrtieFile('')
-    })
-}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { 
+   promtUser().then( (userAnswers) => {
+        //console.log(userAnswers)
+
+         generateMarkdown(userAnswers)
+    })
+ 
+    
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -12,9 +14,31 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  let content = `# ${data.name}`;
 
-`;
+
+  writeToFile('./dist/README.md', content);
+
+}
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  return new Promise((resolve, reject) => {
+      fs.writeFile('./dist/README.md', data, err => {
+          // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+          if (err) {
+              reject(err);
+              // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+              return;
+          }
+
+          // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+          resolve({
+              ok: true,
+              message: 'Success!'
+          })
+      })
+  })
 }
 
 module.exports = generateMarkdown;
